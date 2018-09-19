dc516e98fff9183e35763d5c7345884feb149338
Sean R Lockhart <39708478+SeanRLockhart@users.noreply.github.com>
2018-09-19 16:16:58 -0400

    Update bussiness.java

--- bussiness.java
+++ bussiness.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
-    // TODO: 9/19/2018 Add more comments, please. 
+    // creats list of bissnesses
     public static void main(String[] args) {
         List<String> business = new ArrayList<>();
         business.add( "Jet Loop Methods");
@@ -11,12 +11,13 @@ public class Main {
         business.add("Code Coder Inc.");
         business.add("Express Array");
         business.add("Java Worlds");
-
+//prints out list of buisnesses 
         System.out.println("Name of all the Business list: " + business);
         System.out.println(business);
-
+//method to print array list
         BusinessOutPrint(business);
     }
+    //returns and calls method
     public static List BusinessOutPrint(ArrayList var){
         return  var;
     }
