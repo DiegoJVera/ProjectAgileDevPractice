e2a83bc027084e23dad51cbbbf7015296df907eb
DiegoJVera <DiegoJVera@users.noreply.github.com>
2018-09-19 15:22:49 -0400

    First Submission

---
+++ D.V. v.01/Main.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+import java.util.List;
+
+public static void main() 
+{
+
+    tags(yes);
+
+}
+
+public static ArrayList tags(String decision)
+{
+    List<String> tags = new ArrayList<>();
+    Scanner input = new Scanner(System.in);
+    System.out.println("--------");
+    array.add(input.nextLine());
+    System.out.println("--------");
+
+    return tags;
+}
+
+public static ArrayList tags(boolean )
+
+/*
+public static void input()
+{
+Scanner input = new Scanner(System.in);
+
+    System.out.println(tags);
+
+    scan(tags)
+
+    tags.add("Work-Oriented");
+
+}
+*/
